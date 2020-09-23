782b81eacc5847b1690d5df9ffe3f127c6f681eb
Brad Bryant <bradleybryant86@gmail.com>
2020-09-23 00:22:49 -0500

    Created Image component and imported it to app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Image from './components/image';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> New text.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Murphy & Breedlove Selfcare Products
       </header>
+      <Image />
     </div>
   );
 }
---
+++ src/components/image.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+
+function Image() {
+    return <img src={} alt="logo"></img>
+}
+
+export default Image;
