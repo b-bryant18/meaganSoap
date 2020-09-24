8c5cfb2219a8ad9982adf594ee5cb73d04abad29
Brad Bryant <bradleybryant86@gmail.com>
2020-09-24 00:22:21 -0500

    Created Image component with m&b logo in it

--- src/components/image.js
+++ src/components/image.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Image from 'react-bootstrap/Image';
+import Logo from '../images/logo.png';
 
 function Image() {
-    return <img src={} alt="logo"></img>
+    return <img src={Logo} alt="logo"></img>
 }
 
 export default Image;
