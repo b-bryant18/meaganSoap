0ebe42a297e64a20afd816d5a4e4cfbf72457c7f
Brad Bryant <bradleybryant86@gmail.com>
2020-09-24 00:33:42 -0500

    Changed App class to container

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Wood from './images/wood.jpg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
         <Image />
         Murphy & Breedlove Selfcare Products
