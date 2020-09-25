53185f2d7e4848b5c93db9541d4cf979348e1485
Brad Bryant <bradleybryant86@gmail.com>
2020-09-25 01:17:05 -0500

    Added img tags to Cols and product names to Content component

--- src/components/content.js
+++ src/components/content.js
@@ -5,22 +5,22 @@ function Content() {
     return (
         <Container>
             <Row>
-                <Col> <h5>Product</h5> </Col>
-                <Col> <h5>Product</h5> </Col>
-                <Col> <h5>Product</h5> </Col>
-                <Col> <h5>Product</h5> </Col>
+                <Col><image src={{}} alt="Rose Clay and Honey Drop Soap"><h5>Rose Clay and Honey Drop Soap</h5></image>  </Col>
+                <Col> <image src={{}} alt="Honey, Vanilla, & Oatmeal Goat Milk Soap"><h5>Honey, Vanilla, & Oatmeal Goat Milk Soap</h5></image> </Col>
+                <Col> <image src={{}} alt="Orange & Vanilla Lip Scrub"><h5>Orange & Vanilla Lip Scrub</h5></image> </Col>
+                <Col> <image src={{}} alt="Coffee Beans & Vanilla Goat Milk Soap"><h5>Coffee Beans & Vanilla Goat Milk Soap</h5></image> </Col>
             </Row>
             <Row>
-                <Col> <h5>Product</h5> </Col>
-                <Col> <h5>Product</h5> </Col>
-                <Col> <h5>Product</h5> </Col>
-                <Col> <h5>Product</h5> </Col>
+                <Col> <h5>Lavender Donkey Milk Soap</h5> </Col>
+                <Col> <h5>Mint Lip Scrub</h5> </Col>
+                <Col> <h5>Eucalyptus Goat Milk Soap</h5> </Col>
+                <Col> <h5>Teakwood & Charcoal Shea Butter Soap</h5> </Col>
             </Row>
             <Row>
-                <Col> <h5>Product</h5> </Col>
-                <Col> <h5>Product</h5> </Col>
-                <Col> <h5>Product</h5> </Col>
-                <Col> <h5>Product</h5> </Col>
+                <Col> <h5>Apple Brown Sugar Shea Butter Soap</h5> </Col>
+                <Col> <h5>Bath Tea with Milk Powder, Epsom Salt & Flowers</h5> </Col>
+                <Col> <h5>Raspberry & Lemon Lip Scrub</h5> </Col>
+                <Col> <h5>Lemongrass Drop Goat Milk Soap</h5> </Col>
             </Row>
         </Container>
     )
