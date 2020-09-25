99ad35dff819b1d78658f7234bdb7d0fb99ce597
Brad Bryant <bradleybryant86@gmail.com>
2020-09-25 00:47:56 -0500

    Created Content component and import Container, Row, and Col to make grid in content.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Image from './components/image';
+import Content from './components/content';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Image />
         Murphy & Breedlove Selfcare Products
       </header>
+      <Content></Content>
 
     </div>
   );
---
+++ src/components/content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function Content() {
+    return (
+        <Container>
+            <Row>
+                <Col>.Col </Col>
+            </Row>
+        </Container>
+    )
+}
+export default Content;
