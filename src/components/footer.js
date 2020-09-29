c0abf2c76697582a89a81de84cbe74d128458091
Brad Bryant <bradleybryant86@gmail.com>
2020-09-29 01:04:50 -0500

    Added LinkedIn text in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div class="navbar navbar-inverse navbar-fixed-bottom">
-            <div class="footer">
-                <p class="navbar-text">Copyright Bradley Bryant 2020</p>
-                <a href="https://www.linkedin.com/in/bradley-bryant-194929148/"><i class="fa fa-linkedin"></i></a>
+        <div className="navbar navbar-inverse navbar-fixed-bottom">
+            <div className="footer">
+                <p className="navbar-text">Copyright Bradley Bryant 2020</p>
+                <br />
+                <a href="https://www.linkedin.com/in/bradley-bryant-194929148/"><i className="fa fa-linkedin">LinkedIn</i></a>
             </div>
         </div>
     )
