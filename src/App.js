b3f6d7d8fc61a3cc54069c4cada76dff5fa34d23
Brad Bryant <bradleybryant86@gmail.com>
2020-09-29 00:37:54 -0500

    Created Footer component and added it to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Image from './components/image';
 import Content from './components/content';
+import Footer from './components/footer';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <h4 id="title">Murphy & Breedlove Selfcare Products</h4>
       </header>
       <Content />
-
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div class="navbar navbar-inverse navbar-fixed-bottom">
+            <div class="footer">
+                <p class="navbar-text">Copyright Bradley Bryant 2020</p>
+                <a href="https://www.linkedin.com/in/bradley-bryant-194929148/"><i class="fa fa-linkedin"></i></a>
+            </div>
+        </div>
+    )
+}
+export default Footer;
