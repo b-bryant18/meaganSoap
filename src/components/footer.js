6af78022a178f2377ab2d1f054a5dddafe61842b
Brad Bryant <bradleybryant86@gmail.com>
2020-09-29 01:08:17 -0500

    Centered footer text

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div className="navbar navbar-inverse navbar-fixed-bottom">
-            <div className="footer">
-                <p className="navbar-text">Copyright Bradley Bryant 2020</p>
-                <br />
-                <a href="https://www.linkedin.com/in/bradley-bryant-194929148/"><i className="fa fa-linkedin">LinkedIn</i></a>
-            </div>
+        <div className="footer">
+            <p className="navbar-text">Copyright Bradley Bryant 2020</p>
+            <br />
+            <a href="https://www.linkedin.com/in/bradley-bryant-194929148/"><i className="fa fa-linkedin">LinkedIn</i></a>
         </div>
+
     )
 }
 export default Footer;
